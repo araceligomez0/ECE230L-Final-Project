6e2160f964f8739eb2121b1707264cc683e896f8
araceligomez0 <araceligomezchav@u.boisestate.edu>
2026-04-22 18:29:13 -0600

    Update timer.v

--- timer.v
+++ timer.v
@@ -1,11 +1,11 @@
-// timer.v
+//Timer: Mod-60 downcounter with synchronous load
 module timer(
     input clk,
     input rst,
-    input en,
-    input load,
-    input [5:0] load_value,
-    output reg [5:0] state
+    input en,               //Enables or Disables clock
+    input load,             //If load=1, load the counter with "load_value"
+    input [5:0] load_value, //Value to load into counter register. Counter will then start counting from this value
+    output [5:0] state     //6-bits to represent the highest number 59
 );
 
 initial begin
