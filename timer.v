b6b16335d2cd77bcc6bf7b2adf4345219f575e37
The_Architects727 <100482837+The-Architects727@users.noreply.github.com>
2026-04-30 09:16:12 -0600

    Update timer.v
    Added comments for readability

--- timer.v
+++ timer.v
@@ -1,4 +1,3 @@
-//Timer: Mod-60 downcounter with synchronous load
 module timer(
     input clk,
     input rst,
@@ -8,18 +7,28 @@ module timer(
     output reg [5:0] state     //6-bits to represent the highest number 59
 );
 
+//Simulate starting state of all 0
 initial begin
     state <= 6'd0;
 end
 
+//On positive edge of clock or reset pusle
 always @(posedge clk or posedge rst) begin
-    if (rst)
+    
+		//If reset is active, set state to all 0
+		if (rst)
         state <= 6'd0;
+
+		//If load is active, set state to load value
     else if (load)
         state <= load_value;
+
+		//Else if enable is active, count down.
     else if (en)
         state <= (state == 6'd0) ? 6'd0 : (state - 6'd1);
-    else
+    
+		//If neither load or enable are active, state remains the same.
+		else
         state <= state;
 end
 
