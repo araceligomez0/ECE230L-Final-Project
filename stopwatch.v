123406215433a55191cb9e5421e659e06dd86226
The_Architects727 <100482837+The-Architects727@users.noreply.github.com>
2026-04-30 09:01:28 -0600

    Update stopwatch.v
    Added comments for readability.

--- stopwatch.v
+++ stopwatch.v
@@ -1,4 +1,3 @@
-// stopwatch.v
 module stopwatch(
     input clk,
     input rst,
@@ -6,17 +5,24 @@ module stopwatch(
     output reg [5:0] state //6-bits to represent the highest number 59
 );
 
+//Simulate an all 0 state
 initial begin
     state <= 6'd0;
 end
 
+//On either the Positive edge of clock or reset
 always @(posedge clk or posedge rst) begin
   
+		//If reset is active, set state to all 0
     if (rst)
         state <= 6'd0;
+
+		//Otherwise, If enabled, count up on clock pulse
     else if (en)
         state <= (state == 6'd59) ? 6'd0 : (state + 6'd1);
-    else
+    
+		//If not enabled, state stays the same
+		else
         state <= state;
 end
 
